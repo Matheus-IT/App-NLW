cbacfee5ccc8e11fade415156ca2760b30ce44e3
Matheus <62137767+Matheus-IT@users.noreply.github.com>
2020-06-07 17:21:08 -0300

    I just posted the APP I developed on NLW

---
+++ web/exemples.tsx
@@ -0,0 +1,23 @@
+import React, {/*{ useState }*/} from 'react';
+import './App.css';
+
+// JSX: Sintaxe de XML dentro do JavaScript
+
+function App() {
+  //let [counter, setCounter] = useState(0); // [valor estado, função pra atualizar o valor do estado]
+  /*
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+  */
+  return (
+    <div>
+      { /*
+      <Header title={`Contador:${counter}`} />
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      */ }
+    </div>
+  );
+}
+
+export default App;
